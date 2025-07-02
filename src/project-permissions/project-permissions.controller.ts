a5bfd2a0d1f92b0e51d2ce1b55d08b44972fba7d
Adonis Smlatic <adonis.smlatic@web.de>
2025-07-02 17:42:57 +0200

    Add getProjectPermissions and resetToDefaultProjectPermissions with caching

--- src/project-permissions/project-permissions.controller.ts
+++ src/project-permissions/project-permissions.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Inject, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateProjectPermissionDto } from './dtos/create-project-permission.dto';
 import { CurrentUserId } from 'src/users/decorators/current-user-id.decorator';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { ProjectPermissionsService } from './project-permissions.service';
+import { ProjectPermissionMapDto } from './dtos/project-permission-map.dto';
+import { defaultProjectPermissions } from 'src/config/project-permissions.config';
 
 @Controller('project-permissions')
 export class ProjectPermissionsController {
@@ -41,6 +52,35 @@ export class ProjectPermissionsController {
     await this.cacheManager.set(
       `project-permissions:${projectId}`,
       permissions,
+    );
+  }
+  @Get()
+  async getProjectPermissions(
+    @CurrentUserId() _: string,
+    @Param('projectId') projectId: string,
+  ): Promise<ProjectPermissionMapDto> {
+    const cachedPermissions = await this.cacheManager.get(
+      `project-permissions:${projectId}`,
+    );
+    if (cachedPermissions) return cachedPermissions;
+    const permissions =
+      await this.projectPermissionsService.getProjectPermissions(projectId);
+    await this.cacheManager.set(
+      `project-permissions:${projectId}`,
+      permissions,
+    );
+    return permissions;
+  }
+  @Delete()
+  async resetToDefaultProjectPermissions(
+    @Param('projectId') projectId: string,
+  ): Promise<void> {
+    await this.projectPermissionsService.resetToDefaultProjectPermissions(
+      projectId,
+    );
+    await this.cacheManager.set(
+      `project-permissions:${projectId}`,
+      defaultProjectPermissions,
     );
   }
 }
