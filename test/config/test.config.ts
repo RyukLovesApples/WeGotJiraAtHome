ea7df261ea303fd41acc4bf3b8ebe5fec8c7ea94
Adonis Smlatic <adonis.smlatic@web.de>
2025-07-10 13:36:27 +0200

    Refactor test.config.ts for Docker compatibility and .env setup via bash script

--- test/config/test.config.ts
+++ test/config/test.config.ts
@@ -1,11 +1,11 @@
 export const testConfig = {
   database: {
     type: 'postgres',
-    host: 'db',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'we_got_jira_at_home_integration',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT ?? '5432', 10),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_integration`,
     synchronize: true,
   },
   app: {
