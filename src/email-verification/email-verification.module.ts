c314ee1e28ad3dea79dfce446b5159faa8e8a53c
Adonis Smlatic <adonis.smlatic@web.de>
2025-07-14 13:15:54 +0200

    Add EmailVerification entity and import into module

--- src/email-verification/email-verification.module.ts
+++ src/email-verification/email-verification.module.ts
@@ -3,9 +3,15 @@ import { EmailVerificationService } from './email-verification.service';
 import { EmailVerificationController } from './email-verification.controller';
 import { MailerModule } from 'src/mailer/mailer.module';
 import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmailVerification } from './email-verification.entity';
 
 @Module({
-  imports: [MailerModule, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([EmailVerification]),
+    MailerModule,
+    UsersModule,
+  ],
   providers: [EmailVerificationService],
   controllers: [EmailVerificationController],
 })
